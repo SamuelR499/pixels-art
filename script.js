9e63e48d5e151dd440c53ba0aff2b799082184e1
Samuelr499 <samuelribeiro2@hotmail.com>
2022-03-21 19:02:36 -0300

    conclui requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const quadroPixel = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
 // console.log(quadroPixel);
 
 for (let coluna = 0; coluna < 5; coluna++) {
-  const element = document.createElement('br');
-  quadroPixel.appendChild(element);
+    const element = document.createElement('br');
+    quadroPixel.appendChild(element);
 
-  for (linha = 0; linha < 5; linha++) {
-    const px = document.createElement('div');
-    px.classList = 'pixel';
-    quadroPixel.appendChild(px);
-  }
+    for (linha = 0; linha < 5; linha++) {
+        const px = document.createElement('div');
+        px.classList = 'pixel';
+        quadroPixel.appendChild(px);
+    }
+}
+
+// const redSquare = document.getElementsByClassName('red');
+// const blueSquare = document.getElementsByClassName('blue');
+// const greenSquare = document.getElementsByClassName('green');
+// const blackSquare = document.getElementsByClassName('black');
+// console.log(redSquare);
+
+
+
+colorPalette.addEventListener('click', alteraselected)
+function alteraselected(evnt) {
+    let colorSelec = document.querySelector('.selected');
+    colorSelec.classList.remove('selected')
+    evnt.target.classList.add('selected')
 }
