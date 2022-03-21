17d720d7c38de84ee1d34db8b865cbbbab257fbd
Samuelr499 <samuelribeiro2@hotmail.com>
2022-03-21 16:51:22 -0300

    resolução exercicio 5

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const quadroPixel = document.getElementById('pixel-board');
 // console.log(quadroPixel);
 
+for (let coluna = 0; coluna < 5; coluna++) {
+    const element = document.createElement('br');
+    quadroPixel.appendChild(element)
 
-
-// for(i = 0; i <25; i +=1){
-//     const px = document.createElement('div');
-//     px.classList = 'pixel';
-//     quadroPixel.appendChild(px);
-// }
-for (linha = 1; linha <= 25; linha++) {
-    const px = document.createElement('div');
-    px.classList = 'pixel';
-    quadroPixel.appendChild(px);
+    for (linha = 0; linha < 5; linha++) {
+        const px = document.createElement('div');
+        px.classList = 'pixel';
+        quadroPixel.appendChild(px);
+        
+    }
 }
